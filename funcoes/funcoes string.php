5a80da78ac0a3a9d4e66550a5e3c4f8b9c3d6525
JWeberDEV <1820009@liberato.com.br>
2022-02-21 21:27:13 -0300

    termina aula 16

---
+++ funcoes/funcoes string.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Exercicio While</title>
+</head>
+<body>
+<div>
+    
+    <?php 
+      // $p = "leite";
+      // $pr = 4.5;
+      //printf ("O %s custa R$ %.2f", $p, $pr);  printf é usado para trazer dados ja formatados
+      //print_r serve para mostrar detalhes de uma variável, por exemplo um vetor
+      // $v = array (189,1951,484,18,494);
+      // print_r($v);
+      // echo"<br>";
+      // var_dump($v);
+      // echo"<br>";
+      // echo"<br>";
+      // var_export($v);
+      // $t = "um texto grande aleatorio ai... só pra mostrar a funcao ";
+      // $r = wordwrap($t, 30, "<br/>",false);
+      // echo ($r);
+      // $txt = "josias weber machado";
+      // $tamanho = strlen($txt);
+      // echo ($tamanho);
+      // trim remove todos os espaços em brancos
+      // rtrim remove os espaços do final do texto
+      // str_word_count conta a quantidade de palavras de uma frase ou texto
+      // explode separa as palavras por espaços e poem em um vetor
+      // str_split mostra cada letra de uma palavra em um indice de um vetor
+      // implode funciona da forma oposta do explode
+      // chr(63); serve para mostrar qual a tecla que corresponde ao que esta dentro do parenteses
+      // ord serve para extrair o código de uma tecla digitada.
+    ?>
+</div>
+</body>
+</html>
