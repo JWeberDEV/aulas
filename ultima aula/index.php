cc0b1751e676d8e01a48d1b07e4a8a859b451699
JWeberDEV <1820009@liberato.com.br>
2022-03-01 21:31:47 -0300

    Adiciona a ultima aula com o ultimo conteudo do curso

---
+++ ultima aula/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Exercicio While</title>
+</head>
+<body>
+<div>
+    
+    <?php 
+      // para por um novo valor no final de um vetor, usamos array_push ($n,7);
+      // para eliminar o ultimo elemento do vetor podemos utilizar o array_pop ($n);
+      // para por um novo valor no INICIO de um vetor: array_unshift
+      // para eliminar o PRIMEIRO elemento do vetor podemos utilizar o array_shift
+      
+    ?>
+</div>
+</body>
+</html>
